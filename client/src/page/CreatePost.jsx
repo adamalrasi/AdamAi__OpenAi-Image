32fdeb5e1a92d559f714b1ca7adfac1389e786ba
Adam Alrasi <adam.alrasi@outlook.com>
2024-01-09 20:17:46 +0000

    first commit

---
+++ client/src/page/CreatePost.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
+const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: 'AdamAi',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+       
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+      window.location.reload();
+
+    } else {
+      alert('Please generate an image with proper details');
+    }
+    
+  };
+
+  return (
+    <section className="max-w-4xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-4xl">Create imaginative images of me, AdamAi 😅</h1>
+        <p className="mt-1 text-[#666e75]  max-w-2xl ">Generate images of my pixel doing whatever you like. Whether it's fighting off zombies😱 or riding a dragon🐉, the choice is yours🙌. </p>
+      </div>
+
+      <form onSubmit={handleSubmit}
+      onKeyPress={(event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          generateImage();
+        }
+      }}>
+      <div className='mt-5 max-w-3xl'>
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Example: fighting off ghost 👻👻👻🤺"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+      </div>
+
+        <div className="flex justify-center text-center flex-col">
+          <div className="self-center m-5 border border-[1rem] rounded-xl border-gray-500  p-1">
+            { form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-[15rem] h-[15rem] object-contain "
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className=" object-contain w-[15rem] h-[15rem] "
+              />
+            )}
+
+            {generatingImg && (
+              <div className="">
+                <Loader />
+              </div>
+            )}
+          </div>
+
+        <div className="self-center">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-blue-500 font-medium rounded-md md:mr-5  text-3xl w-full sm:w-auto  h-[4rem] px-4 text-center"
+          >
+            {generatingImg ? 'Creating...' : '🤖 Create'}
+          </button>
+          <button
+            type="submit"
+            className=" text-white bg-gray-500 font-medium md:ml-5 rounded-md text-3xl w-full sm:w-auto h-[4rem] px-8 text-center"
+          >
+            {loading ? 'Saving...' : '👇 Save'}
+          </button>
+        </div>
+        </div>
+    
+      </form>
+    </section>
+  );
+};
+
+export default CreatePost;
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+
+import { Card, FormField, Loader } from '../components';
+
+import { CreatePost } from './index.js'
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return (
+      data.map((post) => <Card key={post._id} {...post} />)
+    );
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
+};
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <CreatePost />
+
+      <div className="mt-[10%]">
+      <h1 className="font-extrabold text-[#222328] text-[32px] mb-5"> Previous AdamAi Generated Images</h1>
+
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+          
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 inline-block">
+                <RenderCards
+                className='my-5  w-64 p-3 h-64'
+                  data={allPosts}
+                  title="No Posts Yet"
+                />
+            
+              
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
